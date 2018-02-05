1eafb4abcc50a4b8c055305d25dc3414ce580848
AshaAgrawal07 <ashaa2@illinois.edu>
2018-02-05 16:37:05 -0600

    added stuff

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.scanner.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner scan = new Scanner(System.in);
+
+	int totalRain = 0;
+	int counter = 0;
+
+	while(scan.hasNext()) {
+	    String rain = scan.nextString();
+	    if (rain.contains("add ")) {
+	        int addRain = rain.nextString(4);
+	        totalRain += rain;
+	        counter ++;
+        } else if (rain.equalsIgnoreCase("average)")) {
+	        System.out.println( totalRain/counter);
+        } else if (rain.equalsIgnoreCase("clear")) {
+	        totalRain = 0;
+	        counter = 0;
+        } else {
+	        System.out.println("Sorry, don't understand that");
+        }
+    }
+
     }
 }
